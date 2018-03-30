c3cec4719779b795beb478f7926cf0bf2db643e4
asdtech153 <asdtech153@live.com>
2018-03-29 21:08:50 -0400

    Initial Commit

---
+++ intro_software_engineering_vitals/intro_software_engineering_vitals/main.cpp
@@ -0,0 +1,22 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+
+	string irregularData [5][5];
+
+	/*Random number generator*/
+
+	/*Number testing*/
+
+	/*isNormal function*/
+
+	/*Send off if*/
+
+		system("pause");
+	return 0;
+}
---
+++ intro_software_engineering_vitals/intro_software_engineering_vitals/user.cpp
@@ -0,0 +1,75 @@
+#include <iomanip>
+#include <iostream>
+
+class user {
+private:
+	 int heartRate;
+	 int userId;
+	 int diastolicBp;
+	 int systolicBp;
+	 int pollingRate;
+	 double skinTemperature;
+	 double oxygenSaturation;
+	 bool warning = false;
+
+public:
+	user();
+	~user();
+	user(int userId, int diastolicBp, int systolicBp,
+		int pollingRate, double skinTemperature, double oxygenSaturation) {
+		this->userId = userId;
+		this->diastolicBp = diastolicBp;
+		this->systolicBp = systolicBp;
+		this->pollingRate = pollingRate;
+		this->skinTemperature = skinTemperature;
+		this->oxygenSaturation = oxygenSaturation;
+	}
+
+	/*Setters*/
+
+	void setHeartRate(int hr) {
+		heartRate = hr;
+	}
+	void setUserId(int uid) {
+		userId = uid;
+	}
+	void setDiastolicBp(int dbp) {
+		diastolicBp = dbp;
+	}
+	void setSystolicBp(int sbp) {
+		systolicBp = sbp;
+	}
+	void setPollingRate(int pr) {
+		pollingRate = pr;
+	}
+	void setSkinTemperature(double st) {
+		skinTemperature = st;
+	}
+	void setOxygenSaturation(double os) {
+		oxygenSaturation = os;
+	}
+	/*Getters*/
+
+	int getHeartRate() {
+		return heartRate;
+	}
+	int getUserId() {
+		return userId;
+	}
+	int getDiastolicBp() {
+		return diastolicBp;
+	}
+	int getSystolicBp() {
+		return systolicBp;
+	}
+	int getPollingRate() {
+		return pollingRate;
+	}
+	double getSkinTemperature() {
+		return skinTemperature;
+	}
+	double getOxygenSaturation() {
+		return oxygenSaturation;
+	}
+};
+
